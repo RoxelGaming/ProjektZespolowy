d541d1e8b429c59a1031d9a30ba33cdc5b50ff79
tester19744-pixel <tester19744@gmail.com>
2026-05-31 17:16:45 +0200

    Update layout.tsx

--- projekt-zespolowy-tiket-bot/app/layout.tsx
+++ projekt-zespolowy-tiket-bot/app/layout.tsx
@@ -37,16 +37,17 @@ export default async function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body>
-        <NextIntlClientProvider messages={messages}>
-          <Navbar>
-            <LanguageSwitcher />
-            <ThemeToggle />
-          </Navbar>
-
-          {children}
-          <AccessibilityPanel />
-        </NextIntlClientProvider>
-      </body>
-    </html>
+        
+         <NextIntlClientProvider messages={messages}>
+           <Navbar>
+             <LanguageSwitcher />
+             <ThemeToggle />
+           </Navbar>
+ 
+           {children}
+           <AccessibilityPanel />
+         </NextIntlClientProvider>
+       </body>
+     </html>
   );
 }
