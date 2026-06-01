332f759a7712ee7a5dbc1156b683092f8c1fa0e3
6 <tester19744@gmail.com>
2026-06-01 19:21:55 +0200

    poprawki plikow .gitignore m package-lock oraz deploy

--- projekt-zespolowy-tiket-bot/next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Jeśli masz to w jakimś podfolderze na serwerze (np. na GitHub Pages lub uczelnianym), wpisz to tu:
-  basePath: process.env.NODE_ENV === 'production' ? '/projektzespolowy' : '',
-  
-  // Reszta Twoich ustawień (np. turbopack)
-};
-
-export default nextConfig;
